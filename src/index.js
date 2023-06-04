4cc0e27204d9f0344d646286489974dd0df6017d
KemalH <kemal7462@gmail.com>
2023-06-04 14:19:43 +0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import Indexes from "./views/Indexes"; /* Main Component */
+import ReactDOM from "react-dom/client";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Indexes /> {/* Main Component */}
   </React.StrictMode>
 );
 
---
+++ src/views/Indexes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Indexes() {
+  return (
+    <>
+      <div className="App">create routing component here</div>
+    </>
+  );
+}
+
+export default Indexes;
